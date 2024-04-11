897641a2b60757a8320f61c06e1ae75ee4536acd
unknown <antonovnikitafgf76@gmail.com>
2024-04-12 00:40:17 +0400

    adding files

---
+++ Nekit107/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import sqlite3
+
+
+def index(request):
+    conn = sqlite3.connect('identifier.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Cargo')
+    data = cursor.fetchall()
+    conn.close()
+    return render(request, 'index.html', {'data': data})
+
+
+def second(request):
+    conn = sqlite3.connect('identifier.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Vehicles')
+    data = cursor.fetchall()
+    conn.close()
+    return render(request, 'second.html', {'data': data})
